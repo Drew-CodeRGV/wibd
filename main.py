cdbc9e63b55e9fb173065dd219e26683f00772ea
Drew Lentz <drew@codergv.org>
2024-11-14 13:11:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,39 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def start():
     return render_template('index.html')
 
-@app.route('/question2', methods=['POST'])
+@app.route('/question2', methods=['GET', 'POST'])
 def question2():
-    if request.form.get('born_in_us'):
+    if request.method == 'POST':
+        if request.form.get('born_in_us') == 'no':
+            return redirect(url_for('results_yes'))
         return render_template('question2.html')
-    return redirect(url_for('results_yes'))
+    return render_template('question2.html')
 
-@app.route('/question3', methods=['POST'])
+@app.route('/question3', methods=['GET', 'POST'])
 def question3():
-    if request.form.get('us_citizen') == 'no':
-        return redirect(url_for('results_yes'))
+    if request.method == 'POST':
+        if request.form.get('us_citizen') == 'no':
+            return redirect(url_for('results_yes'))
+        return render_template('question3.html')
     return render_template('question3.html')
 
-@app.route('/results', methods=['POST'])
+@app.route('/results', methods=['GET', 'POST'])
 def results():
-    if request.form.get('parents_citizens') == 'no':
-        return redirect(url_for('results_yes'))
-    return redirect(url_for('results_no'))
+    if request.method == 'POST':
+        if request.form.get('parents_citizens') == 'no':
+            return redirect(url_for('results_yes'))
+        return redirect(url_for('results_no'))
+    return redirect(url_for('start'))
 
-@app.route('/results_yes')
+@app.route('/results_yes', methods=['GET'])
 def results_yes():
     return render_template('results_yes.html')
 
-@app.route('/results_no')
+@app.route('/results_no', methods=['GET'])
 def results_no():
     return render_template('results_no.html')
 
